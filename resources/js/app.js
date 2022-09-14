b2e5830cd128bde6a9bda5a9210be98f6546606c
JasperBoerhof <jasperboerhof@gmail.com>
2022-09-14 09:44:17 +0200

    Implemented Vue3

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
-import './bootstrap';
+import axios from "axios";
+window.axios = axios;
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+import { createApp } from "vue";
+
+import App from "./App.vue";
+
+const app = createApp(App);
+
+app.mount("#app");
