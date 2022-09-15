28b45c068e475420a14f65efea6bb7d5153c42cb
JasperBoerhof <jasperboerhof@gmail.com>
2022-09-15 10:01:43 +0200

    added example function

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -2,3 +2,10 @@ import { storeModuleFactory } from "./factory/factory";
 
 // Create and export stores based on storeModuleFactory
 export const bookStore = storeModuleFactory("books");
+
+// Example how to add another function to the bookStore, might be buggy as it is not tested
+bookStore.actions.getByCategoryId = async (categoryId) => {
+    const { data } = await axios.get(`books/${categoryId}/category`);
+    if (!data) return;
+    bookStore.setters.setAll(data);
+};
